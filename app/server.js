446296ab5643edd2f30d7bfa63917758047aa995
Sania <sania.cloudtech@gmail.com>
2025-12-06 11:09:31 +0500

    Initial commit - CI/CD Docker Project

---
+++ app/server.js
@@ -0,0 +1,12 @@
+﻿const http = require('http');
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.end("🚀 CI/CD Pipeline with Docker + GitHub Actions — Version 1");
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+
+});
